e3d9901ce5c8690804cdf80b5b5e31abd8ef48a2
RachelKolk <rach.m.obrien@gmail.com>
2019-05-02 16:31:49 -0400

    Sorry for bad commiting - I think I am done with today's mvp

--- src/adv.py
+++ src/adv.py
@@ -44,6 +44,7 @@ room['treasure'].s_to = room['narrow']
 room['foyer'].list.append(item['sword'])
 room['foyer'].list.append(item['sword'])
 
+
 #
 # Main
 #
@@ -53,7 +54,6 @@ current_player = Player("Player1", room['outside'])
 
 # Function that attempts to move the player via the direction they typed
 def move_player(move, current_room):
-    print("moving player")
     if move == "n" and current_room.n_to != "":
         room = current_room.n_to
     elif move == "s" and current_room.s_to != "":
@@ -67,28 +67,63 @@ def move_player(move, current_room):
         return current_room
     return room
     
-
+# Function that allows a player to take or drop an item
+def item_action(action, item , player):
+    if action == "take" or action == "get":
+        for x in player.current_room.list:
+            if x.name.lower() == item.lower():
+                player.inventory.append(x)
+                player.current_room.list.remove(x)
+                print(x.on_take())
+    if action == "drop":
+        for x in player.inventory:
+            if x.name.lower() == item.lower():
+                player.inventory.remove(x)
+                player.current_room.list.append(x)
+                print(x.on_drop())
+    return player
 
 # Write a loop that:
 while True:
     # * Prints the current room name
+    # additional empty prints a hacky way of better spacing - looking into how to do this better...
+    print()
     print(current_player.current_room.name)
-    
+    print()
     # * Prints the current description (the textwrap module might be useful here).
     print(current_player.current_room.description)
+    print()
 
     # * Prints the items that are visible to the player in the room they are in
+    print("You see the following objects of interest:")
     for x in current_player.current_room.list:
         print(x)
 
     # * Waits for user input and decides what to do.
-    move = input("\n Please enter a cardinal direction. (Ex: North) \n Or a q to quit the game. >>").lower()[0]        
-    # If the user enters "q", quit the game.
-    if move == "q":
-        print(" Thanks for playing!")
-        break    
-    # If the user enters a cardinal direction, attempt to move to the room there.
-    # Print an error message if the movement isn't allowed.
-    current_player.current_room = move_player(move, current_player.current_room)
-    print("\n")
+    cmd = input("\n Please enter a direction you'd like to explore. (Ex: North) \n\n Or you can 'take' an object in the room and add it to your inventory \n Or 'drop' one of your inventory items. \n\n Enter i to check your inventory \n Or enter q to quit the game. >>").split(" ")    
+    # The below if statement checks to see if the input is one word
+    if len(cmd) == 1:
+        move = cmd[0].lower()
+        # If the user enters "q", quit the game.
+        if move[0] == "q":
+            print()
+            print(" Thanks for playing! \n")
+            break
+
+        # If the user enters "i", their inventory will print to the console.
+        if move[0] == "i":
+            print("\n The items in your inventory are:")
+            current_player.player_items()
+
+        # If the user enters a cardinal direction, attempt to move to the room there.
+        # Print an error message if the movement isn't allowed.        
+        else:
+            current_player.current_room = move_player(move[0], current_player.current_room)
+            print("\n")
+    
+    # Checks to see if the input is 2 words
+    # Then deals with inventory actions (function above - lines 70-84)
+    if len(cmd) == 2:
+        current_player = item_action(cmd[0], cmd[1], current_player)
+
    
--- src/player.py
+++ src/player.py
@@ -5,7 +5,16 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+        self.inventory = []
         
 
     def __str__(self):
-        return f"{self.current_room}"
+        return f"{self.current_room}, {self.inventory}"
+
+    def player_items(self):
+        if len(self.inventory) == 0:
+            print("\t nothing!!!")
+        for x in self.inventory:
+            print("\t" + x.name+": " + x.description)
+        # only for line spacing
+        print()
